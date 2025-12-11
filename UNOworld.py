a79636a8234fe77ee2502964129c739449e6c5db
Sesuu2003 <ulisescheveste@gmail.com>
2025-12-11 17:06:00 -0300

    Update UNOworld.py

--- UNOworld.py
+++ UNOworld.py
@@ -13,28 +13,29 @@ class UNOEnvironment(SimulatedEnvironment):
         self._hands = {}
         self._deck = [] #mazo de cartas
         self._discard_pile = []
-        self._current_player_index = 0
+        self._current_player_index = 0 #indice de la lista de agentes. Indica de cual es el turno.
         self._direction = 1 #sentido de la ronda (1 o -1)
         self._current_color = None
 
     def add(self, agent_id: int) -> None:
         super(UNOEnvironment, self).add(agent_id)
-        self._agents_locations[agent_id] = 0
+        # añadir al final del index
+        self._agent_ids.append(agent_id)
 
     def remove(self, agent_id: int) -> None:
         super(UNOEnvironment, self).remove(agent_id)
-        self._agents_locations.pop(agent_id, None)
+        self._agents_ids.pop(agent_id, None)
 
-    def add_statebuffer(self, agent_id: int, statebuffer: IStateBuffer) -> None:
-        super(UNOEnvironment, self).add_statebuffer(agent_id, statebuffer)
-        statebuffer.update({"length": self._length, "agent_location": self._location_of(agent_id),
-                         "dirt_location": self._dirt_locations})
+    #def add_statebuffer(self, agent_id: int, statebuffer: IStateBuffer) -> None:
+    #    super(UNOEnvironment, self).add_statebuffer(agent_id, statebuffer)
+    #    statebuffer.update({"length": self._length, "agent_location": self._location_of(agent_id),
+    #                     "dirt_location": self._dirt_locations})
 
     def remove_statebuffer(self, agent_id: int,statebuffer: IStateBuffer) -> None:
         super(UNOEnvironment, self).remove_statebuffer(agent_id, statebuffer)
 
-    def _location_of(self, agent_id: int) -> int:
-        return self._agents_locations[agent_id] if agent_id in self._agents_locations else None
+    #def _location_of(self, agent_id: int) -> int:
+    #    return self._agents_locations[agent_id] if agent_id in self._agents_locations else None
 
     def get_property(self, agent_id: int, property_name: str) -> dict:
         if agent_id in self._agent_ids:
@@ -43,7 +44,7 @@ class UNOEnvironment(SimulatedEnvironment):
             property_methods = {
                 "hand": self._hands[agent_id],
                 "your_turn": agent_id== self._agent_ids[self._current_player_index],
-                "top_discard": self._discardpile[-1],
+                "top_discard": self._discard_pile[-1],
                 "current_color": self._current_color,
                 
             }
@@ -60,21 +61,42 @@ class UNOEnvironment(SimulatedEnvironment):
             return {}
         
     def _play_card(self, agent_id:int, card):
-        #validar, mover al descarte, aplicar sus efectos
+        #validar carta
+        # mover al descarte
+        # aplicar sus efectos
+        if card.type == "SKIP":
+            self._advance_turn(steps=2)
+        elif card.type == "REVERSE":
+            self._direction *= -1 #cambia el sentido
+            self._advance_turn(steps=1)
+        elif card.type == "+2":
+            self.make_next_draw_cards(2) #id del siguiente jugador
+            self._advance_turn(steps=2)
+        elif card.type == "+4":
+            self.make_next_draw_cards(4) #id del siguiente jugador
+            self._advance_turn(steps=2)
+
+        else:
+            self._advance_turn(steps=1)
+        
+    def _make_next_draw_cards(self, amount):
+        next_index = (self._current_player_index + self._direction)
+        next_player = self._agent_ids[next_index]
+        self._draw_card(self, next_player, amount)
+        
+    def _draw_card(self, agent_id:int, amount: int):
+        for i in range(amount):
+            card = self._deck.pop()
+            self._hands[agent_id].append(card)
         self._advance_turn()
         
-    def _draw_card(self, agent_id:int):
-        card = self._deck.pop()
-        self._hands[agent_id].append(card)
-        self._advance_turn()
-        
-    def _advance_turn(self):
+    def _advance_turn(self, steps: int):
         self._current_player_index = (self._current_player_index + self._direction)
 
     def take_action(self, agent_id: int, action_name: str, params: dict = {}) -> None:
         if agent_id in self._agents:
             action_methods = {
-                "play_card": (self._play_card(agent_id, params["card"])),
+                "play_card": (self._play_card, ["card"]),
                 "draw_card":(self._draw_card(agent_id)),
                 "declare_color":(self._current_color = params["color"]),
                 "pass":(self._advance_turn())
